4e7954fb794d69591c66ceba84b676380c5d0da4
thiagohc20 <thiago-henrique40@hotmail.com>
2022-10-31 23:05:41 -0300

    Adicionado função no botao de excluir todos os itens

--- js/script.js
+++ js/script.js
@@ -2,16 +2,20 @@ const adicionar = document.querySelector(".adicionar");
 const article = document.querySelector(".item-container");
 const info = document.querySelector(".input-item");
 
-
 adicionar.addEventListener("click", adicionarItem);
+
+let infoAtual;
 let editFlag = false;
+
 function adicionarItem(e) {
-  const valor = info.value;
   e.preventDefault();
+  //Guarda dados do input
+  const valor = info.value;
+  //Funcao para verificar se o input esta vazio e o editFlag verdadeiro
   if (valor !== "" && !editFlag) {
     const div = document.createElement("div");
     div.innerHTML = `<article class="item-list">
-        <p class="item-value">${info.value}</p>
+        <p class="item-value">${valor}</p>
         <div class="item-buttons">
             <button type="button" class="editar"><img src="./assets/editar.png" alt="" srcset=""></button>
             <button type="button" class="excluir"><img src="./assets/excluir.png" alt="" srcset=""></button>
@@ -20,22 +24,23 @@ function adicionarItem(e) {
 
     article.appendChild(div);
     valoresPadrão();
-    if (div) {
-      //Botao de Remover
-      const botaoRemover = document.querySelectorAll(".excluir");
-      botaoRemover.forEach((item) => item.addEventListener("click", remover));
-      //Botao de Editar
-    }
-  }else if(valor !== "" && editFlag === true){
+    //Botao de excluir um item
+    const botaoRemover = document.querySelectorAll(".excluir");
+    botaoRemover.forEach((item) => item.addEventListener("click", remover));
+    //Botao de editar
+    const botaoEditar = document.querySelectorAll(".editar");
+    botaoEditar.forEach((item) => item.addEventListener("click", editar));
+    //Botao excluir todos os itens
+    const botaoExcluirTodosItens = document.querySelector(".limpar-btn")
+    botaoExcluirTodosItens.addEventListener('click', excluirTodosOsItens)
+
+  } else if (valor !== "" && editFlag === true) {
     //Altera a informação do item
-    article.querySelector('p').innerText = info.value
+    infoAtual.innerHTML = valor;
     //Retorna para os valores padrao
     valoresPadrão();
   }
-  const botaoEditar = document.querySelectorAll(".editar");
-  botaoEditar.forEach((item) => item.addEventListener("click", editar));
 }
-
 
 //Define os valores padrao
 function valoresPadrão() {
@@ -51,11 +56,21 @@ function remover(e) {
 }
 
 //Função de Editar Itens
-function editar() {
+function editar(e) {
+  infoAtual = e.currentTarget.parentElement.parentElement.querySelector("p");
   //Define true para ser editado
-  editFlag = true
-  //Altera o valor de "adicione" para a informação atual do texto
-  info.value = article.querySelector('p').innerText
+  editFlag = true;
+  //Altera o input de "adicione" para a informação atual do texto
+  info.value = infoAtual.innerHTML;
   //Muda o nome do botao adicionar
   adicionar.value = "Editar";
+}
+
+//Função de excluir todos os itens
+function excluirTodosOsItens() {
+  //Guarda todo conteudo de item container
+  const todosOsItens = document.querySelector('.item-container')
+  //Seleciona todos os itens e remove
+  todosOsItens.querySelectorAll('div').forEach((item) => item.remove())
+  valoresPadrão();
 }
